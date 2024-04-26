a80cd8496f8a81b573bda2d0730df219788fa78e
Harnoor <sahniharnoor101@gmail.com>
2024-04-26 13:12:14 -0700

    commiting files

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -11,15 +11,14 @@ public class FamilyTree
     
     private static class TreeNode
     {
-        private String                    name;
+        private String                  name;
         private TreeNode                parent;
-        private ArrayList<TreeNode>        children;
-        
-        
+        private ArrayList<TreeNode>      children;
         TreeNode(String name)
         {
             this.name = name;
             children = new ArrayList<>();
+            
         }
         
         
@@ -33,6 +32,9 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	this.children.add(childNode);
+        	childNode.parent = this;
+        	
         }
         
         
@@ -41,7 +43,7 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (this.getName() == targetName)
                 return this;
                     
             // No, recurse. Check all children of this node.
@@ -49,6 +51,9 @@ public class FamilyTree
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+            	if (child.getNodeWithName(targetName) != null) {
+            		return child;
+            	}
             }
             
             // Not found anywhere.
@@ -66,6 +71,14 @@ public class FamilyTree
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
+            TreeNode curr = this;
+            
+            while (curr!= null) {
+            	ancestors.add(curr);
+            	if (curr.parent != null) {
+            		curr = curr.parent;
+            	}
+            }
 
             return ancestors;
         }
@@ -210,3 +223,5 @@ public class FamilyTree
 		}
 	}
 }
+
+
